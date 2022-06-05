25f7ae3ac85c2a0c4ae7352f69c3dccb9f48d99b
highkickk <222831284@qq.com>
2022-06-04 20:32:58 -0700

    Post likes and delete

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,8 +3,12 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
+import { addLike, removeLike, deletePost } from '../../actions/posts';
 
 const PostItem = ({
+  addLike,
+  removeLike,
+  deletePost,
   auth,
   post: { _id, text, name, avatar, user, likes, comments, date },
 }) => {
@@ -22,15 +26,19 @@ const PostItem = ({
           <p class='post-date'>
             Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
           </p>
-          <button type='button' class='btn btn-light'>
+          <button
+            onClick={(e) => addLike(_id)}
+            type='button'
+            class='btn btn-light'
+          >
             <i class='fas fa-thumbs-up'></i>{' '}
-            <span>
-              {likes.length > 0 && (
-                <span class='comment-count'>{likes.length}</span>
-              )}
-            </span>
+            <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
           </button>
-          <button type='button' class='btn btn-light'>
+          <button
+            onClick={(e) => removeLike(_id)}
+            type='button'
+            class='btn btn-light'
+          >
             <i class='fas fa-thumbs-down'></i>
           </button>
           <Link to={`/post/${_id}`} class='btn btn-primary'>
@@ -39,8 +47,12 @@ const PostItem = ({
               <span class='comment-count'>{comments.length}</span>
             )}
           </Link>
-          {!auth.loading && user === auth.user_id && (
-            <button type='button' class='btn btn-danger'>
+          {!auth.loading && user === auth.user._id && (
+            <button
+              onClick={(e) => deletePost(_id)}
+              type='button'
+              class='btn btn-danger'
+            >
               <i class='fas fa-times'></i>
             </button>
           )}
@@ -53,9 +65,15 @@ const PostItem = ({
 PostItem.propTypes = {
   posts: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired,
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired,
+  deletePost: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   auth: state.auth,
 });
-export default connect(mapStateToProps, {})(PostItem);
+
+export default connect(mapStateToProps, { addLike, removeLike, deletePost })(
+  PostItem
+);
